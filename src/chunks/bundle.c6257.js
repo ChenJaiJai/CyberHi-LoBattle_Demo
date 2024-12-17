7487e19ae80bed2ec8f737a51e59e7d678dfdc25
JaiJai <wm.rd01@gmail.com>
2024-12-17 10:03:20 +0800

    耍大牌Demo0.0.1

---
+++ src/chunks/bundle.c6257.js
@@ -0,0 +1,587 @@
+System.register([], function(_export, _context) { return { execute: function () {
+System.register("chunks:///_virtual/_rollupPluginModLoBabelHelpers.js", [], function (exports) {
+  'use strict';
+
+  return {
+    execute: function () {
+      exports({
+        AsyncGenerator: _AsyncGenerator,
+        AwaitValue: _AwaitValue,
+        applyDecoratedDescriptor: _applyDecoratedDescriptor,
+        arrayLikeToArray: _arrayLikeToArray,
+        assertThisInitialized: _assertThisInitialized,
+        asyncIterator: _asyncIterator,
+        asyncToGenerator: _asyncToGenerator,
+        construct: _construct,
+        createClass: _createClass,
+        createForOfIteratorHelperLoose: _createForOfIteratorHelperLoose,
+        defineProperty: _defineProperty,
+        extends: _extends,
+        getPrototypeOf: _getPrototypeOf,
+        inheritsLoose: _inheritsLoose,
+        initializerDefineProperty: _initializerDefineProperty,
+        isNativeFunction: _isNativeFunction,
+        isNativeReflectConstruct: _isNativeReflectConstruct,
+        setPrototypeOf: _setPrototypeOf,
+        unsupportedIterableToArray: _unsupportedIterableToArray,
+        wrapAsyncGenerator: _wrapAsyncGenerator,
+        wrapNativeSuper: _wrapNativeSuper
+      });
+
+      function _asyncIterator(iterable) {
+        var method;
+
+        if (typeof Symbol !== "undefined") {
+          if (Symbol.asyncIterator) {
+            method = iterable[Symbol.asyncIterator];
+            if (method != null) return method.call(iterable);
+          }
+
+          if (Symbol.iterator) {
+            method = iterable[Symbol.iterator];
+            if (method != null) return method.call(iterable);
+          }
+        }
+
+        throw new TypeError("Object is not async iterable");
+      }
+
+      function _AwaitValue(value) {
+        this.wrapped = value;
+      }
+
+      function _AsyncGenerator(gen) {
+        var front, back;
+
+        function send(key, arg) {
+          return new Promise(function (resolve, reject) {
+            var request = {
+              key: key,
+              arg: arg,
+              resolve: resolve,
+              reject: reject,
+              next: null
+            };
+
+            if (back) {
+              back = back.next = request;
+            } else {
+              front = back = request;
+              resume(key, arg);
+            }
+          });
+        }
+
+        function resume(key, arg) {
+          try {
+            var result = gen[key](arg);
+            var value = result.value;
+            var wrappedAwait = value instanceof _AwaitValue;
+            Promise.resolve(wrappedAwait ? value.wrapped : value).then(function (arg) {
+              if (wrappedAwait) {
+                resume(key === "return" ? "return" : "next", arg);
+                return;
+              }
+
+              settle(result.done ? "return" : "normal", arg);
+            }, function (err) {
+              resume("throw", err);
+            });
+          } catch (err) {
+            settle("throw", err);
+          }
+        }
+
+        function settle(type, value) {
+          switch (type) {
+            case "return":
+              front.resolve({
+                value: value,
+                done: true
+              });
+              break;
+
+            case "throw":
+              front.reject(value);
+              break;
+
+            default:
+              front.resolve({
+                value: value,
+                done: false
+              });
+              break;
+          }
+
+          front = front.next;
+
+          if (front) {
+            resume(front.key, front.arg);
+          } else {
+            back = null;
+          }
+        }
+
+        this._invoke = send;
+
+        if (typeof gen.return !== "function") {
+          this.return = undefined;
+        }
+      }
+
+      if (typeof Symbol === "function" && Symbol.asyncIterator) {
+        _AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+          return this;
+        };
+      }
+
+      _AsyncGenerator.prototype.next = function (arg) {
+        return this._invoke("next", arg);
+      };
+
+      _AsyncGenerator.prototype.throw = function (arg) {
+        return this._invoke("throw", arg);
+      };
+
+      _AsyncGenerator.prototype.return = function (arg) {
+        return this._invoke("return", arg);
+      };
+
+      function _wrapAsyncGenerator(fn) {
+        return function () {
+          return new _AsyncGenerator(fn.apply(this, arguments));
+        };
+      }
+
+      function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {
+        try {
+          var info = gen[key](arg);
+          var value = info.value;
+        } catch (error) {
+          reject(error);
+          return;
+        }
+
+        if (info.done) {
+          resolve(value);
+        } else {
+          Promise.resolve(value).then(_next, _throw);
+        }
+      }
+
+      function _asyncToGenerator(fn) {
+        return function () {
+          var self = this,
+              args = arguments;
+          return new Promise(function (resolve, reject) {
+            var gen = fn.apply(self, args);
+
+            function _next(value) {
+              asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);
+            }
+
+            function _throw(err) {
+              asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);
+            }
+
+            _next(undefined);
+          });
+        };
+      }
+
+      function _defineProperties(target, props) {
+        for (var i = 0; i < props.length; i++) {
+          var descriptor = props[i];
+          descriptor.enumerable = descriptor.enumerable || false;
+          descriptor.configurable = true;
+          if ("value" in descriptor) descriptor.writable = true;
+          Object.defineProperty(target, descriptor.key, descriptor);
+        }
+      }
+
+      function _createClass(Constructor, protoProps, staticProps) {
+        if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+        if (staticProps) _defineProperties(Constructor, staticProps);
+        return Constructor;
+      }
+
+      function _defineProperty(obj, key, value) {
+        if (key in obj) {
+          Object.defineProperty(obj, key, {
+            value: value,
+            enumerable: true,
+            configurable: true,
+            writable: true
+          });
+        } else {
+          obj[key] = value;
+        }
+
+        return obj;
+      }
+
+      function _extends() {
+        _extends = exports('extends', Object.assign || function (target) {
+          for (var i = 1; i < arguments.length; i++) {
+            var source = arguments[i];
+
+            for (var key in source) {
+              if (Object.prototype.hasOwnProperty.call(source, key)) {
+                target[key] = source[key];
+              }
+            }
+          }
+
+          return target;
+        });
+        return _extends.apply(this, arguments);
+      }
+
+      function _inheritsLoose(subClass, superClass) {
+        subClass.prototype = Object.create(superClass.prototype);
+        subClass.prototype.constructor = subClass;
+
+        _setPrototypeOf(subClass, superClass);
+      }
+
+      function _getPrototypeOf(o) {
+        _getPrototypeOf = exports('getPrototypeOf', Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
+          return o.__proto__ || Object.getPrototypeOf(o);
+        });
+        return _getPrototypeOf(o);
+      }
+
+      function _setPrototypeOf(o, p) {
+        _setPrototypeOf = exports('setPrototypeOf', Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+          o.__proto__ = p;
+          return o;
+        });
+        return _setPrototypeOf(o, p);
+      }
+
+      function _isNativeReflectConstruct() {
+        if (typeof Reflect === "undefined" || !Reflect.construct) return false;
+        if (Reflect.construct.sham) return false;
+        if (typeof Proxy === "function") return true;
+
+        try {
+          Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {}));
+          return true;
+        } catch (e) {
+          return false;
+        }
+      }
+
+      function _construct(Parent, args, Class) {
+        if (_isNativeReflectConstruct()) {
+          _construct = exports('construct', Reflect.construct);
+        } else {
+          _construct = exports('construct', function _construct(Parent, args, Class) {
+            var a = [null];
+            a.push.apply(a, args);
+            var Constructor = Function.bind.apply(Parent, a);
+            var instance = new Constructor();
+            if (Class) _setPrototypeOf(instance, Class.prototype);
+            return instance;
+          });
+        }
+
+        return _construct.apply(null, arguments);
+      }
+
+      function _isNativeFunction(fn) {
+        return Function.toString.call(fn).indexOf("[native code]") !== -1;
+      }
+
+      function _wrapNativeSuper(Class) {
+        var _cache = typeof Map === "function" ? new Map() : undefined;
+
+        _wrapNativeSuper = exports('wrapNativeSuper', function _wrapNativeSuper(Class) {
+          if (Class === null || !_isNativeFunction(Class)) return Class;
+
+          if (typeof Class !== "function") {
+            throw new TypeError("Super expression must either be null or a function");
+          }
+
+          if (typeof _cache !== "undefined") {
+            if (_cache.has(Class)) return _cache.get(Class);
+
+            _cache.set(Class, Wrapper);
+          }
+
+          function Wrapper() {
+            return _construct(Class, arguments, _getPrototypeOf(this).constructor);
+          }
+
+          Wrapper.prototype = Object.create(Class.prototype, {
+            constructor: {
+              value: Wrapper,
+              enumerable: false,
+              writable: true,
+              configurable: true
+            }
+          });
+          return _setPrototypeOf(Wrapper, Class);
+        });
+        return _wrapNativeSuper(Class);
+      }
+
+      function _assertThisInitialized(self) {
+        if (self === void 0) {
+          throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+        }
+
+        return self;
+      }
+
+      function _unsupportedIterableToArray(o, minLen) {
+        if (!o) return;
+        if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+        var n = Object.prototype.toString.call(o).slice(8, -1);
+        if (n === "Object" && o.constructor) n = o.constructor.name;
+        if (n === "Map" || n === "Set") return Array.from(o);
+        if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+      }
+
+      function _arrayLikeToArray(arr, len) {
+        if (len == null || len > arr.length) len = arr.length;
+
+        for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+        return arr2;
+      }
+
+      function _createForOfIteratorHelperLoose(o, allowArrayLike) {
+        var it;
+
+        if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) {
+          if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {
+            if (it) o = it;
+            var i = 0;
+            return function () {
+              if (i >= o.length) return {
+                done: true
+              };
+              return {
+                done: false,
+                value: o[i++]
+              };
+            };
+          }
+
+          throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+        }
+
+        it = o[Symbol.iterator]();
+        return it.next.bind(it);
+      }
+
+      function _initializerDefineProperty(target, property, descriptor, context) {
+        if (!descriptor) return;
+        Object.defineProperty(target, property, {
+          enumerable: descriptor.enumerable,
+          configurable: descriptor.configurable,
+          writable: descriptor.writable,
+          value: descriptor.initializer ? descriptor.initializer.call(context) : void 0
+        });
+      }
+
+      function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) {
+        var desc = {};
+        Object.keys(descriptor).forEach(function (key) {
+          desc[key] = descriptor[key];
+        });
+        desc.enumerable = !!desc.enumerable;
+        desc.configurable = !!desc.configurable;
+
+        if ('value' in desc || desc.initializer) {
+          desc.writable = true;
+        }
+
+        desc = decorators.slice().reverse().reduce(function (desc, decorator) {
+          return decorator(target, property, desc) || desc;
+        }, desc);
+
+        if (context && desc.initializer !== void 0) {
+          desc.value = desc.initializer ? desc.initializer.call(context) : void 0;
+          desc.initializer = undefined;
+        }
+
+        if (desc.initializer === void 0) {
+          Object.defineProperty(target, property, desc);
+          desc = null;
+        }
+
+        return desc;
+      }
+    }
+  };
+});
+
+System.register("chunks:///_virtual/EditSpine.mjs_cjs=&original=.js", ['./cjs-loader.mjs', './EditSpine.js'], function (exports, module) {
+  'use strict';
+
+  var loader, __cjsMetaURL;
+
+  return {
+    setters: [function (module) {
+      loader = module.default;
+    }, function (module) {
+      __cjsMetaURL = module.__cjsMetaURL;
+      var _setter = {};
+      _setter.__cjsMetaURL = module.__cjsMetaURL;
+      _setter.default = module.default;
+      exports(_setter);
+    }],
+    execute: function () {
+      // I am the facade module who provides access to the CommonJS module './EditSpine.js'~
+      if (!__cjsMetaURL) {
+        loader.throwInvalidWrapper('./EditSpine.js', module.meta.url);
+      }
+
+      loader.require(__cjsMetaURL);
+    }
+  };
+});
+
+System.register("chunks:///_virtual/cjs-loader.mjs", [], function (exports) {
+  'use strict';
+
+  return {
+    execute: function () {
+      var CjsLoader = /*#__PURE__*/function () {
+        function CjsLoader() {
+          this._namedWrappers = {};
+          this._resolveCache = {};
+          this._moduleCache = {};
+        }
+
+        var _proto = CjsLoader.prototype;
+
+        _proto.define = function define(id, wrapper) {
+          this._namedWrappers[id] = wrapper;
+        };
+
+        _proto.require = function require(id) {
+          return this._require(id);
+        };
+
+        _proto.createRequireWithReqMap = function createRequireWithReqMap(requireMap, originalRequire) {
+          return function (specifier) {
+            var resolved = requireMap[specifier];
+
+            if (resolved) {
+              return originalRequire(resolved);
+            } else {
+              throw new Error('Unresolved specifier ' + specifier);
+            }
+          };
+        };
+
+        _proto.throwInvalidWrapper = function throwInvalidWrapper(requestTarget, from) {
+          throw new Error("Module '" + requestTarget + "' imported from '" + from + "' is expected be an ESM-wrapped CommonJS module but it doesn't.");
+        };
+
+        _proto._require = function _require(id, parent) {
+          var cachedModule = this._moduleCache[id];
+
+          if (cachedModule) {
+            return cachedModule.exports;
+          }
+
+          var module = {
+            id: id,
+            exports: {}
+          };
+          this._moduleCache[id] = module;
+
+          this._tryModuleLoad(module, id);
+
+          return module.exports;
+        };
+
+        _proto._resolve = function _resolve(specifier, parent) {
+          return this._resolveFromInfos(specifier, parent) || this._throwUnresolved(specifier, parent);
+        };
+
+        _proto._resolveFromInfos = function _resolveFromInfos(specifier, parent) {
+          var _cjsInfos$parent$reso, _cjsInfos$parent;
+
+          if (specifier in cjsInfos) {
+            return specifier;
+          }
+
+          if (!parent) {
+            return;
+          }
+
+          return (_cjsInfos$parent$reso = (_cjsInfos$parent = cjsInfos[parent]) === null || _cjsInfos$parent === void 0 ? void 0 : _cjsInfos$parent.resolveCache[specifier]) !== null && _cjsInfos$parent$reso !== void 0 ? _cjsInfos$parent$reso : undefined;
+        };
+
+        _proto._tryModuleLoad = function _tryModuleLoad(module, id) {
+          var threw = true;
+
+          try {
+            this._load(module, id);
+
+            threw = false;
+          } finally {
+            if (threw) {
+              delete this._moduleCache[id];
+            }
+          }
+        };
+
+        _proto._load = function _load(module, id) {
+          var wrapper = this._loadWrapper(id);
+
+          var require = this._createRequire(module);
+
+          wrapper(module.exports, require, module);
+        };
+
+        _proto._loadWrapper = function _loadWrapper(id) {
+          if (id in this._namedWrappers) {
+            return this._namedWrappers[id];
+          } else {
+            return this._loadExternalWrapper(id);
+          }
+        };
+
+        _proto._loadExternalWrapper = function _loadExternalWrapper(id) {
+          return function (exports) {
+            var path;
+
+            try {
+              path = URL.fileURLToPath(id);
+            } catch (err) {
+              throw new Error(id + " is not a valid file URL");
+            }
+
+            var extern = require(path);
+
+            Object.assign(exports, extern);
+          };
+        };
+
+        _proto._createRequire = function _createRequire(module) {
+          var _this = this;
+
+          return function (specifier) {
+            return _this._require(specifier, module);
+          };
+        };
+
+        _proto._throwUnresolved = function _throwUnresolved(specifier, parentUrl) {
+          throw new Error("Unable to resolve " + specifier + " from " + parent + ".");
+        };
+
+        return CjsLoader;
+      }();
+
+      var loader = exports('default', new CjsLoader());
+    }
+  };
+});
+
+} }; });
