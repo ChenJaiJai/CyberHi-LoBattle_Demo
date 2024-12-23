1f5f6012289f42288b73f939f6ae53c2226c180f
JaiJai <wm.rd01@gmail.com>
2024-12-23 16:54:04 +0800

    test

--- index.4a536.js
+++ index.4a536.js
@@ -1,4 +1,4 @@
-System.register(["./application.915df.js"], function (_export, _context) {
+System.register(["./application.8f1af.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
