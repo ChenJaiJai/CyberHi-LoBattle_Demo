d64f3152b223156c5361d7e892ba337f85bdbcf3
JaiJai <wm.rd01@gmail.com>
2024-12-24 14:55:39 +0800

    找到原因了，重新上板，並且修改給出答案時不會有平局問題

--- index.45a67.js
+++ index.45a67.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.b8b5e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
