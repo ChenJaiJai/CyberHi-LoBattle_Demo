1a20456d7c5a73b8f3e6e3f2542576d8aadbc57d
JaiJai <wm.rd01@gmail.com>
2024-12-25 17:51:12 +0800

    上傳0.0.3
    耍大牌對決 0.0.3更新
    turbo新增三階段，取消disble效果改成prass效果，新增持續旋轉，轉速為1->2->4倍速
    AutoPlay新增選注功能
    chip修改成改色功能(暫時)，並在表演期間無法點擊
    改大量底層

--- application.40c21.js
+++ application.40c21.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.187b1.json';
+          this.settingsPath = 'src/settings.80d06.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
